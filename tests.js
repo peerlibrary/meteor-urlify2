7abaa2dc861bb31ea1a4504bf8f790132e6da00c
Mitar <mitar.git@tnode.com>
2014-02-07 00:03:19 -0800

    When limiting the length, split on word breaks.

--- tests.js
+++ tests.js
@@ -11,4 +11,7 @@ Tinytest.add('meteor-urlify2', function (test) {
 
   test.equal(URLify2('Işık ılık süt iç'), 'isik-ilik-sut-ic');
   test.equal(URLify2('ČĆŽŠĐ čćžšđ'), 'cczsdj-cczsdj');
+  test.equal(URLify2('-word-'), 'word');
+  test.equal(URLify2('long test with many words', 10), 'long-test');
+  test.equal(URLify2('onelongwordtoolongformaxlength', 10), 'onelongwor');
 });
--- urlify2.js
+++ urlify2.js
@@ -1,12 +1,26 @@
 URLify2 = function (s, num_chars) {
-	s = URLify2.downcode(s);
-	s = URLify2.nfd(s).replace(/[^\u0000-\u00FF]/g, ""); // convert to ascii
-	s = s.replace(/[^\w\s-]/g, ''); // remove unneeded chars
-	s = s.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
-	s = s.replace(/[-\s]+/g, '-'); // convert spaces to hyphens
-	s = s.toLowerCase(); // convert to lowercase
+    s = URLify2.downcode(s);
+    s = URLify2.nfd(s).replace(/[^\u0000-\u00FF]/g, ""); // Convert to ASCII
+    s = s.replace(/[^\w\s-]/g, ''); // Remove unwanted characters
+    s = s.replace(/^\s+|\s+$/g, ''); // Trim leading/trailing dashes
+    s = s.replace(/[-\s]+/g, '-'); // Convert spaces to dashes
+    s = s.replace(/^-+|-+$/g, ''); // Trim leading/trailing dashes
+    s = s.toLowerCase();
     if (num_chars) {
-        return s.substring(0, num_chars); // trim to first num_chars chars
+        if (s.length <= num_chars) {
+            return num_chars;
+        }
+        var splits = s.split('-');
+        if (splits.length === 1) {
+            return s.substring(0, num_chars);
+        }
+        else {
+            s = splits.shift();
+            while (splits[0] && s.length + splits[0].length + 1 <= num_chars) {
+                s += '-' + splits.shift();
+            }
+            return s;
+        }
     }
     else {
         return s;
