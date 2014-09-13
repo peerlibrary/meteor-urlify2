7455aa1aed8058ea6c382097eaa02e95226614a7
Mitar <mitar.git@tnode.com>
2014-09-13 11:00:21 -0700

    Migration to Meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
-  summary: "Generate slugs with smart urlify supporting unicode"
+  summary: "Generate slugs with smart urlify supporting unicode",
+  version: '0.2.2',
+  name: 'peerlibrary:urlify2',
+  git: 'https://github.com/peerlibrary/meteor-urlify2.git'
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.1.1');
+
   api.export('URLify2');
 
   api.add_files([
@@ -14,6 +19,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use(['urlify2', 'tinytest', 'test-helpers'], ['client', 'server']);
+  api.use(['peerlibrary:urlify2', 'tinytest', 'test-helpers'], ['client', 'server']);
+
   api.add_files('tests.js', ['client', 'server']);
 });
